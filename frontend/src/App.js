3f786d49f624ec5009810121497872fe22479342
Bruno - DEA <81820719+brunnosilllva@users.noreply.github.com>
2025-06-03 21:47:00 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import FocosCalorApp from './components/FocosCalorApp';
+import './index.css';
 
+function App() {
+  return (
+    <div className="App">
+      {/* Componente principal do sistema de focos de calor */}
+      <FocosCalorApp />
+      
+      {/* Footer global (opcional) */}
+      <div className="sr-only">
+        Sistema de monitoramento de focos de calor do Maranhão - IMESC
+      </div>
+    </div>
+  );
+}
+
+export default App;
